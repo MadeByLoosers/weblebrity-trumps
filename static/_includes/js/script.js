483ebb8a06e2a6f21349294eb19f9e367e9add9b
k0nG <mailmattcollins@gmail.com>
2012-09-15 15:49:34 +0100

    Start of game logic. Comparing stats

--- static/_includes/js/script.js
+++ static/_includes/js/script.js
@@ -4,9 +4,10 @@
 
 var WT = WT || {};
 WT.weblebrities = [];
-WT.player1 = { name: '', cards: []};
-WT.player2 = { name: '', cards: []};
-
+WT.player1 = { name: 'Player1', cards: [], el: $('#player1')};
+WT.player2 = { name: 'Player2', cards: [], el: $('#player2')};
+WT.cardTemplate = '<div class="card"><h2><%= name %></h2><dl></dl></div>';
+WT.statTemplate = '<dt data-stat="<%= name %>"><%= name %></dt><dd data-stat="<%= name %>"><%= value %><dd>';
 
 $(document).ready(function () {
 
@@ -42,5 +43,59 @@ WT.init = function(){
 		WT.player1.cards = _.first(WT.weblebrities, cardsEach);
 		WT.player2.cards = _.rest(WT.weblebrities, cardsEach);
 
+		WT.showCard(WT.player1);
+		WT.startRound(WT.player1);
 	});
-}
+};
+
+WT.showCard = function(player){
+
+	var card = player.cards[0];
+
+	var output = $(_.template(WT.cardTemplate, {name: card.name}));
+
+	_.each(card.stats, function(value, key){
+		var stat = {name: key, value: value};
+		output.find('dl').append(_.template(WT.statTemplate, stat));
+	});
+	console.log(player.el);
+	player.el.append(output);
+	
+};
+
+WT.startRound = function(player){
+
+	player.el.find('dl dt, dl dd').on('click', function(e){
+		var stat = $(this).attr('data-stat');
+		WT.compareCards(stat);
+		WT.showCard(WT.player2);
+	});
+};
+
+WT.compareCards = function(stat){
+
+	var winner,
+		draw = false;
+
+	var player1Value = WT.player1.cards[0].stats[stat];
+	var player2Value = WT.player2.cards[0].stats[stat];
+	
+	switch(stat){
+		case 'twitter':
+		case 'linkedin':
+		case 'github':
+		case 'lanyrd':
+		case 'facebook':
+			//Higher value wins.
+			if(player1Value > player2Value){
+				winner = WT.player1
+			}else if(player1Value < player2Value){
+				winner = WT.player2
+			}else if(player1Value == player2Value){
+				draw = true;
+			}
+	}
+
+	$('header').html('Winner: '+winner.name);
+	
+};
