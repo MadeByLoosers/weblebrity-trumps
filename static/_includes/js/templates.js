25a3d2fcd634c8912e88fb7007e2b92ff973bd35
John Lund <john@lundesign.co.uk>
2013-01-12 13:08:51 +0000

    Added class for initial big circle

--- static/_includes/js/templates.js
+++ static/_includes/js/templates.js
@@ -2,7 +2,7 @@
 var WT = WT || {};
 WT.templates = {};
 
-WT.templates.startTemplate = '<div id="start"><a href="" class="player" data-players="1">One Player</a><a href="" class="player" data-players="2">Two Players</a></div>';
-WT.templates.playerTemplate = '<div class="player" id="player<%= number %>"><h1>Player <%= number %><h1></div>';	
+WT.templates.startTemplate = '<div id="start" class="big-circle"><a href="" class="player" data-players="1">One Player</a><a href="" class="player" data-players="2">Two Players</a></div>';
+WT.templates.playerTemplate = '<div class="player" id="player<%= number %>"><h1>Player <%= number %><h1></div>';
 WT.templates.cardTemplate = '<div class="card"><h2><%= name %></h2><dl></dl></div>';
 WT.templates.statTemplate = '<dt data-stat="<%= name %>"><%= name %></dt><dd data-stat="<%= name %>"><%= value %><dd>';
